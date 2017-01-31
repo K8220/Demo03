c8dba9b19efe8301ea3ebbc71b28622ab25f4b86
Esa Lehtinen <potatti@outlook.com>
2017-01-31 18:36:06 +0200

    Oliot final

---
+++ KelloApplication/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelloApplication
+{
+    class Clock
+    {
+        public int hours { get; set; }
+        public int minutes { get; set; }
+        public bool alarm { get; set; }
+        public bool wakeup { get; set; }
+        public string tod { get; set; }
+
+        public void bell()
+        {
+            if (alarm == true)
+            {
+                Console.WriteLine("ARMAGEDDON");
+            }
+        }
+
+        public void sleep5()
+        {
+            minutes += 5;
+        }
+
+        public void sleep15()
+        {
+            minutes += 15;
+        }
+
+        public void press()
+        {
+            alarm = false;
+        }
+    }
+}
---
+++ KelloApplication/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelloApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.hours = 6;
+            clock.minutes = 00;
+            int select = 1;
+
+            while (clock.wakeup == false)
+            {
+                Console.WriteLine(clock.hours + " : " + clock.minutes);
+                Console.WriteLine("What do?");
+                Console.WriteLine("1. Sleep 5 mins");
+                Console.WriteLine("2. Sleep 15 mins");
+                if (clock.alarm == true)
+                {
+                    if (clock.hours >= 8)
+                    {
+                        Console.WriteLine("It ran out of batteries...");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("3. Shut the alarm!");
+                    }
+                }
+                
+                string line = Console.ReadLine();
+                select = int.Parse(line);
+                switch (select)
+                {
+                    case 1:
+                        clock.sleep5();
+                        break;
+                    case 2:
+                        clock.sleep15();
+                        break;
+                    case 3:
+                        clock.press();
+                        System.Environment.Exit(1);
+                        break;
+                }
+                if (clock.minutes >= 60)
+                {
+                    clock.minutes = (clock.minutes - 60);
+                    clock.hours = (clock.hours + 1);
+                }
+                if (clock.hours >= 7)
+                {
+                    clock.alarm = true;
+                    clock.bell();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
